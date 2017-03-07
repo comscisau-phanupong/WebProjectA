51513ff60b12872cfc8a2b84fa33b478f14b8d01
comscisau-phanupong <s5752410036@sau.ac.th>
2017-03-07 16:28:32 +0700

    Study DB 5752410036

---
+++ index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Document</title>
+</head>
+<body>
+	<center>
+		<h1>Home Pag</h1>
+		<hr>
+		<form action="signinpocss.php" method="post">
+		User name : <input type="text" name="uname">
+		<br><br>
+		Passwor : <input type="password" name="upassword">
+		<br><br>
+		<input type="submit" value="Sing in">
+		<input type="reset" value="Cancel">	
+		
+		</form>
+		<a href="registermember.php">ลงทะเบียน</a>		
+	</center>
+</body>
+</html>
